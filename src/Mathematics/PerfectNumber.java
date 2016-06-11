45da064f22ac7c7246f3d0549ac8adb1a32f21e0
kkmonlee <kkmonlee@gmail.com>
2016-06-11 02:51:43 +0100

    Created Perfect Number class

---
+++ src/Mathematics/PerfectNumber.java
@@ -0,0 +1,36 @@
+package Mathematics;
+
+/**
+ * Natural number n is perfect, if it's value is equal to a sum of all its positive
+ * divisors.
+ */
+public class PerfectNumber {
+    /**
+     * Checks whether the number is perfect
+     * @param number tested number
+     * @return true if the number is perfect, false otherwise
+     */
+    public static boolean isPerfect(long number) {
+        // Odd perfect number does not exist
+        // If it does, it has a higher value than long can represent
+        if (number % 2 == 1)
+            return false;
+
+        long result = 1; // trivial
+        long i = 2;
+        while (i * i <= number) { // until i <= sqrt(number)
+            if (number % i == 0) {
+                result += i;
+                result += number / i;
+            }
+            i++;
+        }
+        if (i * i == number) {
+            // perfect square, sqrt(number) was added twice
+            result -= i;
+        }
+
+        return result == number;
+    }
+
+}
