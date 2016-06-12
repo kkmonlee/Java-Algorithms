8c108be8c82dbc17a83a2849efae52ae093c8cdf
kkmonlee <kkmonlee@gmail.com>
2016-06-12 02:24:04 +0100

    Created Prim's Algorithm package
    and it's classes :v:

---
+++ src/Graphs/PrimsAlgorithm/IndexMinPQ.java
@@ -0,0 +1,328 @@
+package Graphs.PrimsAlgorithm;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+/**
+ * This class represents an indexed priority queue of generic keys.
+ * Supports the usual insert and delete-the-minimum operations
+ * along with a few more.
+ *
+ * @author kkmonlee
+ * @param <Key> the generic type of key on this priority queue
+ */
+public class IndexMinPQ<Key extends Comparable<Key>> implements Iterable<Integer> {
+    private int maxN;           // maximum number of elements on PQ
+    private int N;              // number of elements on PQ
+    private int[] pq;           // binary heap using 1-based indexing
+    private int[] qp;           //inverse of pq - qp[pq[i]] = pq[qp[i]] = i
+    private Key[] keys;         // keys[i] = priority of i
+
+    /**
+     * Initialises an empty indexed priority queue with indices between 0 and maxN - 1
+     * @param maxN the keys on this priority queue are index from 0 to maxN - 1
+     * @throws IllegalArgumentException if maxN < 0
+     */
+    public IndexMinPQ(int maxN) {
+        if (maxN < 0) throw new IllegalArgumentException();
+        this.maxN = maxN;
+
+        keys    = (Key[]) new Comparable[maxN + 1];
+        pq      = new int[maxN + 1];
+        qp      = new int[maxN + 1];
+        for (int i = 0; i <= maxN; i++) {
+            qp[i] = -1;
+        }
+    }
+
+    /**
+     * Returns true if this priority queue is empty.
+     * @return true if PQ is empty
+     *         false otherwise
+     */
+    public boolean isEmpty() {
+        return N == 0;
+    }
+
+    /**
+     * Is i an index on this priority queue?
+     * @param i an index
+     * @return true if i is an index, false otherwise
+     * @throws IndexOutOfBoundsException unless 0 <= i < maxN
+     */
+    public boolean contains(int i) {
+        if (i < 0 || i >= maxN) throw new IndexOutOfBoundsException();
+
+        return qp[i] != -1;
+    }
+
+    /**
+     * Returns the number of keys on this priority queue.
+     *
+     * @return the number of keys on this priority queue
+     */
+    public int size() {
+        return N;
+    }
+
+    /**
+     * Associates key with index <tt>i</tt>.
+     *
+     * @param  i an index
+     * @param  key the key to associate with index <tt>i</tt>
+     * @throws IndexOutOfBoundsException unless 0 &le; <tt>i</tt> &lt; <tt>maxN</tt>
+     * @throws IllegalArgumentException if there already is an item associated
+     *         with index <tt>i</tt>
+     */
+    public void insert(int i, Key key) {
+        if (i < 0 || i >= maxN) throw new IndexOutOfBoundsException();
+        if (contains(i)) throw new IllegalArgumentException("index is already in the priority queue");
+        N++;
+        qp[i] = N;
+        pq[N] = i;
+        keys[i] = key;
+        swim(N);
+    }
+
+    /**
+     * Returns an index associated with a minimum key.
+     *
+     * @return an index associated with a minimum key
+     * @throws NoSuchElementException if this priority queue is empty
+     */
+    public int minIndex() {
+        if (N == 0) throw new NoSuchElementException("Priority queue underflow");
+        return pq[1];
+    }
+
+    /**
+     * Returns a minimum key.
+     *
+     * @return a minimum key
+     * @throws NoSuchElementException if this priority queue is empty
+     */
+    public Key minKey() {
+        if (N == 0) throw new NoSuchElementException("Priority queue underflow");
+        return keys[pq[1]];
+    }
+
+    /**
+     * Removes a minimum key and returns its associated index.
+     * @return an index associated with a minimum key
+     * @throws NoSuchElementException if this priority queue is empty
+     */
+    public int delMin() {
+        if (N == 0) throw new NoSuchElementException("Priority queue underflow");
+        int min = pq[1];
+        exch(1, N--);
+        sink(1);
+        assert min == pq[N+1];
+        qp[min] = -1;        // delete
+        keys[min] = null;    // to help with garbage collection
+        pq[N+1] = -1;        // not needed
+        return min;
+    }
+
+    /**
+     * Returns the key associated with index <tt>i</tt>.
+     *
+     * @param  i the index of the key to return
+     * @return the key associated with index <tt>i</tt>
+     * @throws IndexOutOfBoundsException unless 0 &le; <tt>i</tt> &lt; <tt>maxN</tt>
+     * @throws NoSuchElementException no key is associated with index <tt>i</tt>
+     */
+    public Key keyOf(int i) {
+        if (i < 0 || i >= maxN) throw new IndexOutOfBoundsException();
+        if (!contains(i)) throw new NoSuchElementException("index is not in the priority queue");
+        else return keys[i];
+    }
+
+    /**
+     * Change the key associated with index <tt>i</tt> to the specified value.
+     *
+     * @param  i the index of the key to change
+     * @param  key change the key associated with index <tt>i</tt> to this key
+     * @throws IndexOutOfBoundsException unless 0 &le; <tt>i</tt> &lt; <tt>maxN</tt>
+     * @throws NoSuchElementException no key is associated with index <tt>i</tt>
+     */
+    public void changeKey(int i, Key key) {
+        if (i < 0 || i >= maxN) throw new IndexOutOfBoundsException();
+        if (!contains(i)) throw new NoSuchElementException("index is not in the priority queue");
+        keys[i] = key;
+        swim(qp[i]);
+        sink(qp[i]);
+    }
+
+    /**
+     * Change the key associated with index <tt>i</tt> to the specified value.
+     *
+     * @param  i the index of the key to change
+     * @param  key change the key associated with index <tt>i</tt> to this key
+     * @throws IndexOutOfBoundsException unless 0 &le; <tt>i</tt> &lt; <tt>maxN</tt>
+     * @deprecated Replaced by {@link #changeKey(int, Key)}.
+     */
+    public void change(int i, Key key) {
+        changeKey(i, key);
+    }
+
+    /**
+     * Decrease the key associated with index <tt>i</tt> to the specified value.
+     *
+     * @param  i the index of the key to decrease
+     * @param  key decrease the key associated with index <tt>i</tt> to this key
+     * @throws IndexOutOfBoundsException unless 0 &le; <tt>i</tt> &lt; <tt>maxN</tt>
+     * @throws IllegalArgumentException if key &ge; key associated with index <tt>i</tt>
+     * @throws NoSuchElementException no key is associated with index <tt>i</tt>
+     */
+    public void decreaseKey(int i, Key key) {
+        if (i < 0 || i >= maxN) throw new IndexOutOfBoundsException();
+        if (!contains(i)) throw new NoSuchElementException("index is not in the priority queue");
+        if (keys[i].compareTo(key) <= 0)
+            throw new IllegalArgumentException("Calling decreaseKey() with given argument would not strictly decrease the key");
+        keys[i] = key;
+        swim(qp[i]);
+    }
+
+    /**
+     * Increase the key associated with index <tt>i</tt> to the specified value.
+     *
+     * @param  i the index of the key to increase
+     * @param  key increase the key associated with index <tt>i</tt> to this key
+     * @throws IndexOutOfBoundsException unless 0 &le; <tt>i</tt> &lt; <tt>maxN</tt>
+     * @throws IllegalArgumentException if key &le; key associated with index <tt>i</tt>
+     * @throws NoSuchElementException no key is associated with index <tt>i</tt>
+     */
+    public void increaseKey(int i, Key key) {
+        if (i < 0 || i >= maxN) throw new IndexOutOfBoundsException();
+        if (!contains(i)) throw new NoSuchElementException("index is not in the priority queue");
+        if (keys[i].compareTo(key) >= 0)
+            throw new IllegalArgumentException("Calling increaseKey() with given argument would not strictly increase the key");
+        keys[i] = key;
+        sink(qp[i]);
+    }
+
+    /**
+     * Remove the key associated with index <tt>i</tt>.
+     *
+     * @param  i the index of the key to remove
+     * @throws IndexOutOfBoundsException unless 0 &le; <tt>i</tt> &lt; <tt>maxN</tt>
+     * @throws NoSuchElementException no key is associated with index <t>i</tt>
+     */
+    public void delete(int i) {
+        if (i < 0 || i >= maxN) throw new IndexOutOfBoundsException();
+        if (!contains(i)) throw new NoSuchElementException("index is not in the priority queue");
+        int index = qp[i];
+        exch(index, N--);
+        swim(index);
+        sink(index);
+        keys[i] = null;
+        qp[i] = -1;
+    }
+
+
+    /***************************************************************************
+     * General helper functions.
+     ***************************************************************************/
+    private boolean greater(int i, int j) {
+        return keys[pq[i]].compareTo(keys[pq[j]]) > 0;
+    }
+
+    private void exch(int i, int j) {
+        int swap = pq[i];
+        pq[i] = pq[j];
+        pq[j] = swap;
+        qp[pq[i]] = i;
+        qp[pq[j]] = j;
+    }
+
+
+    /***************************************************************************
+     * Heap helper functions.
+     ***************************************************************************/
+    private void swim(int k) {
+        while (k > 1 && greater(k/2, k)) {
+            exch(k, k/2);
+            k = k/2;
+        }
+    }
+
+    private void sink(int k) {
+        while (2*k <= N) {
+            int j = 2*k;
+            if (j < N && greater(j, j+1)) j++;
+            if (!greater(k, j)) break;
+            exch(k, j);
+            k = j;
+        }
+    }
+
+
+    /***************************************************************************
+     * Iterators.
+     ***************************************************************************/
+
+    /**
+     * Returns an iterator that iterates over the keys on the
+     * priority queue in ascending order.
+     * The iterator doesn't implement <tt>remove()</tt> since it's optional.
+     *
+     * @return an iterator that iterates over the keys in ascending order
+     */
+    public Iterator<Integer> iterator() { return new HeapIterator(); }
+
+    private class HeapIterator implements Iterator<Integer> {
+        // create a new pq
+        private IndexMinPQ<Key> copy;
+
+        // add all elements to copy of heap
+        // takes linear time since already in heap order so no keys move
+        public HeapIterator() {
+            copy = new IndexMinPQ<Key>(pq.length - 1);
+            for (int i = 1; i <= N; i++)
+                copy.insert(pq[i], keys[pq[i]]);
+        }
+
+        public boolean hasNext()  { return !copy.isEmpty();                     }
+        public void remove()      { throw new UnsupportedOperationException();  }
+
+        public Integer next() {
+            if (!hasNext()) throw new NoSuchElementException();
+            return copy.delMin();
+        }
+    }
+
+
+    /**
+     * Unit tests the <tt>IndexMinPQ</tt> data type.
+     */
+    public static void main(String[] args) {
+        // insert a bunch of strings
+        String[] strings = { "it", "was", "the", "best", "of", "times", "it", "was", "the", "worst" };
+
+        IndexMinPQ<String> pq = new IndexMinPQ<String>(strings.length);
+        for (int i = 0; i < strings.length; i++) {
+            pq.insert(i, strings[i]);
+        }
+
+        // delete and print each key
+        while (!pq.isEmpty()) {
+            int i = pq.delMin();
+            StdOut.println(i + " " + strings[i]);
+        }
+        StdOut.println();
+
+        // reinsert the same strings
+        for (int i = 0; i < strings.length; i++) {
+            pq.insert(i, strings[i]);
+        }
+
+        // print each key using the iterator
+        for (int i : pq) {
+            StdOut.println(i + " " + strings[i]);
+        }
+        while (!pq.isEmpty()) {
+            pq.delMin();
+        }
+
+    }
+
+}
